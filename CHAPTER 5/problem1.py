51b731146fb84cc4643e44a6c671019c9faed168
Rahul Raj <rahultime2018@gmail.com>
2024-12-16 23:45:27 +0530

    .

--- CHAPTER 5/problem1.py
+++ CHAPTER 5/problem1.py
@@ -1,7 +1,7 @@
 #WAP to create a dictionary of Hindi words with values as their English translation. Provide user with an option to look it up
 
 words = {
-    "Madad" : "Help",
+    "Madad" : "Help", 
     "Kursi" : "Chair",
     "Billi" : "Cat",
     "Ghar" : "Home"
